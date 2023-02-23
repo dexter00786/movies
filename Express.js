e54898fb12288c48a906a4c8aeaaddb6d88eab12
= <sachin.kumar@mobcoder.com>
2023-02-23 17:12:01 +0530

    feat:updateFile function, posters

--- Express.js
+++ Express.js
@@ -1,12 +1,15 @@
 const express = require("express"); // import export module
 const app = express(); //stores object returned by express function()
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const fs = require("fs");
+const fsp = require("fs").promises;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // function to update file
-const updateFile = (array, message, res) => {
+const updateFileOne = (array, message, res) => {
   fs.writeFile("movies.json", JSON.stringify(array), (err) => {
     if (err != null) {
       res.send({ message: "there is an error!" });
@@ -15,43 +18,85 @@ const updateFile = (array, message, res) => {
   });
 };
 
+const updateFileTwo = async (array) => {
+  let flag;
+  await fsp
+    .writeFile("movies.json", JSON.stringify(array))
+    .then(() => {
+      flag = true;
+    })
+    .catch((err) => {
+      console.log(err);
+      flag = false;
+    });
+  return flag;
+};
+
 //reading files
 const moviesJson = fs.readFileSync("movies.json", { encoding: "utf-8" });
 let movies = JSON.parse(moviesJson);
 
 //get all movies
-app.get("/movies", (req, res) => {
+app.get("/api/v1/movies", (req, res) => {
   res.send(movies);
 });
 
 // get movies by id
-app.get("/movies/:id", (req, res) => {
+app.get("/api/v1/movies/:id", (req, res) => {
   let movie = movies.find((ele) => {
     return ele.id == req.params.id;
   });
   // check for movie
-  if (!movie) {
+  if (movie == undefined) {
     res.send({ message: "Movie not found" });
   }
   res.send(movie);
 });
 
 // post movies (add movie to the list)
-app.post("/movies", (req, res) => {
+app.post("/api/v1/movies", async (req, res) => {
   let movie = req.body;
   movies.push(movie);
-  updateFile(movies, "Movie has been successfully added!", res);
+  let status = await updateFileTwo(movies);
+  if (status) {
+    res.send({ message: "Movie has been successfully added!" });
+  } else {
+    res.send({ error: "There is an error while adding movie!" });
+  }
 });
 
 // delete a movie by id
-app.delete("/movies/:id", (req, res) => {
+app.delete("/api/v1/movies/:id", async (req, res) => {
   let id = req.params.id;
-  movies.forEach((ele, index) => {
+  let movie = movies.forEach((ele, index) => {
     if (ele.id == id) {
       movies.splice(index, 1);
     }
   });
-  updateFile(movies, "Movie has been successfully deleted", res);
+  let status = await updateFileTwo(movies);
+  if (status) {
+    res.send({ message: "Movie has been successfully deleted!" });
+  } else {
+    res.send({ error: "There is an error while deleting movie!" });
+  }
+});
+
+//update an existing movie
+app.put("/api/v1/movies/:id", async (req, res) => {
+  let id = req.params.id;
+  let movie = req.body;
+  movies.forEach((ele, index) => {
+    if (ele.id == id) {
+      ele.name = movie.name;
+      ele.revenue = movie.revenue;
+    }
+  });
+  let status = await updateFileTwo(movies);
+  if (status) {
+    res.send({ message: "Movie has been successfully updated!" });
+  } else {
+    res.send({ error: "There is an error while updating movie!" });
+  }
 });
 
 const port = 3000;
